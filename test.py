3bf6c21c00964a585110c15cde0027d693f8b302
lotco <kgsin7@gmail.com>
2019-11-26 22:46:33 +0900

    예제 하나 추가

--- test.py
+++ test.py
@@ -26,3 +26,28 @@ print(session.run([a, b, c]))
 
 # 세션을 닫습니다.
 session.close()
+
+
+#https://gist.github.com/haje01/202ac276bace4b25dd3f
+
+# 변수를 0으로 초기화
+state = tf.Variable(0, name="counter")
+
+# state에 1을 더할 오퍼레이션 생성
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# 그래프는 처음에 변수를 초기화해야 합니다. 아래 함수를 통해 init 오퍼레이션을 만듭니다.   
+init_op = tf.initialize_all_variables()
+
+# 그래프를 띄우고 오퍼레이션들을 실행
+with tf.Session() as sess:
+  # 초기화 오퍼레이션 실행
+  sess.run(init_op)
+  # state의 초기 값을 출력
+  print(sess.run(state))
+  # state를 갱신하는 오퍼레이션을 실행하고, state를 출력
+  for _ in range(3):
+    sess.run(update)
+    print(sess.run(state))
